c6fd5c44eace2113db7f1da6d75251ef35a4e0a7
VmX5 <SockMonkeyboy@hotmail.com>
2017-03-08 18:01:10 -0500

    Initial Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(337, 130)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(113, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Generate Wallpaper"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 43)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(436, 81)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Wallpaper Settings"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(398, 46)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(25, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 51)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Monitor Wallpaper :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(246, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Monitor Size :"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Monitor Name :"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(115, 48)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(277, 20)
+        Me.TextBox3.TabIndex = 10
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(95, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(145, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(323, 23)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.AutoScroll = True
+        Me.FlowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(12, 12)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(436, 25)
+        Me.FlowLayoutPanel1.TabIndex = 7
+        Me.FlowLayoutPanel1.WrapContents = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(459, 241)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "MultiMonitor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents FlowLayoutPanel1 As System.Windows.Forms.FlowLayoutPanel
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,142 @@
+﻿Public Class Form1
+
+    Dim CurrentSelectedMonitor As MonitorBox
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim largestHeight As Integer = 0
+        For i As Integer = 0 To SystemInformation.MonitorCount - 1
+            Dim nMonitor As New MonitorBox
+            nMonitor.Size = New Size((15 / 100) * Screen.AllScreens(i).WorkingArea.Width, (15 / 100) * Screen.AllScreens(i).WorkingArea.Height)
+            nMonitor.MonitorSize = New Size(Screen.AllScreens(i).WorkingArea.Width, Screen.AllScreens(i).WorkingArea.Height)
+            nMonitor.MonitorName = Screen.AllScreens(i).DeviceName.ToString
+            nMonitor.MonitorWorkingArea = Screen.AllScreens(i).WorkingArea.Location
+            AddHandler nMonitor.Click, AddressOf MonitorBoxClicked
+            AddHandler nMonitor.DoubleClick, AddressOf MonitorBoxDoubleClicked
+            FlowLayoutPanel1.Controls.Add(nMonitor)
+            If Screen.AllScreens(i).WorkingArea.Height > largestHeight Then
+                largestHeight = Screen.AllScreens(i).WorkingArea.Height
+            End If
+        Next
+        FlowLayoutPanel1.Size = New Size(FlowLayoutPanel1.Width, ((15 / 100) * largestHeight) + 8)
+        GroupBox1.Location = New Point(12, FlowLayoutPanel1.Location.Y + FlowLayoutPanel1.Height + 7)
+        Button2.Location = New Point(337, GroupBox1.Location.Y + GroupBox1.Size.Height + 4)
+        Me.Size = New Size(Width, Button2.Location.Y + Button2.Height + 47)
+    End Sub
+
+    Public Function ScaleImage(ByVal OldImage As Image, ByVal TargetHeight As Integer, ByVal TargetWidth As Integer) As System.Drawing.Image
+        Dim NewHeight As Integer = TargetHeight
+        Dim NewWidth As Integer = NewHeight / OldImage.Height * OldImage.Width
+        If NewWidth > TargetWidth Then
+            NewWidth = TargetWidth
+            NewHeight = NewWidth / OldImage.Width * OldImage.Height
+        End If
+        Return New Bitmap(OldImage, NewWidth, NewHeight)
+    End Function
+
+    Public Sub MonitorBoxClicked(sender As Object, e As EventArgs)
+        Dim curMonitor As MonitorBox = DirectCast(sender, MonitorBox)
+        CurrentSelectedMonitor = curMonitor
+        TextBox1.Text = curMonitor.MonitorName
+        TextBox2.Text = curMonitor.MonitorSize.Width.ToString & "x" & curMonitor.MonitorSize.Height.ToString
+        TextBox3.Text = curMonitor.MonitorWallpaper
+    End Sub
+
+    Public Sub MonitorBoxDoubleClicked(sender As Object, e As EventArgs)
+        Dim curMonitor As MonitorBox = DirectCast(sender, MonitorBox)
+        CurrentSelectedMonitor = curMonitor
+        TextBox1.Text = curMonitor.MonitorName
+        TextBox2.Text = curMonitor.MonitorSize.Width.ToString & "x" & curMonitor.MonitorSize.Height.ToString
+        TextBox3.Text = curMonitor.MonitorWallpaper
+
+        Button1.PerformClick()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ofd As New OpenFileDialog
+        If ofd.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim nowImg As Image = Image.FromFile(ofd.FileName)
+            CurrentSelectedMonitor.Image = Image.FromFile(ofd.FileName)
+            CurrentSelectedMonitor.MonitorWallpaper = ofd.FileName.ToString
+            TextBox3.Text = ofd.FileName
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim genWallpaperImage As New Bitmap(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height)
+        For Each ctrl As Control In FlowLayoutPanel1.Controls
+            If TypeOf ctrl Is MonitorBox Then
+                Dim checkMon As MonitorBox = DirectCast(ctrl, MonitorBox)
+                If String.IsNullOrEmpty(checkMon.MonitorWallpaper) Then
+                    Dim result = MessageBox.Show(String.Format("Monitor {0} Is missing a wallpaper, If you choose to continue monitor {0} will have a black background!", checkMon.MonitorName.ToString), "Monitor Missing!", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error)
+                    If result = DialogResult.Cancel Then
+                    ElseIf result = DialogResult.No Then
+                    ElseIf result = DialogResult.Yes Then
+                        GoTo genWallpaper
+                    End If
+                Else
+genWallpaper:
+                    Dim g As Graphics = Graphics.FromImage(genWallpaperImage)
+                    g.Clear(Color.Black)
+
+                    For Each nMon As MonitorBox In FlowLayoutPanel1.Controls
+                        g.DrawImage(Image.FromFile(nMon.MonitorWallpaper), New Rectangle(nMon.MonitorWorkingArea.X, nMon.MonitorWorkingArea.Y, nMon.MonitorSize.Width, nMon.MonitorSize.Height))
+                    Next
+
+                End If
+            End If
+        Next
+
+        Dim sfd As New SaveFileDialog
+        If sfd.ShowDialog = Windows.Forms.DialogResult.OK Then
+            genWallpaperImage.Save(sfd.FileName)
+        End If
+    End Sub
+End Class
+
+Public Class MonitorBox
+    Inherits PictureBox
+
+    Public _MonitorName As String
+    Public _MontiorSize As Size
+    Public _MonitorWallpaper As String
+    Public _MonitorWorkingArea As Point
+
+    Public Property MonitorWorkingArea As Point
+        Get
+            Return _MonitorWorkingArea
+        End Get
+        Set(value As Point)
+            _MonitorWorkingArea = value
+        End Set
+    End Property
+
+    Public Property MonitorWallpaper As String
+        Get
+            Return _MonitorWallpaper
+        End Get
+        Set(value As String)
+            _MonitorWallpaper = value
+        End Set
+    End Property
+    Public Property MonitorName As String
+        Get
+            Return _MonitorName
+        End Get
+        Set(value As String)
+            _MonitorName = value
+        End Set
+    End Property
+    Public Property MonitorSize As Size
+        Get
+            Return _MontiorSize
+        End Get
+        Set(value As Size)
+            _MontiorSize = value
+        End Set
+    End Property
+
+    Sub New()
+        SizeMode = PictureBoxSizeMode.StretchImage
+        BorderStyle = Windows.Forms.BorderStyle.FixedSingle
+    End Sub
+End Class
